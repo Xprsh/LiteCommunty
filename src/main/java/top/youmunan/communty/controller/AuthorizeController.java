69a258c08aacf8826ecaf6d6168cb41e5c4d56b8
Xprsh <zhuxprsh@sina.com>
2020-07-08 21:10:24 +0800

    update

--- src/main/java/top/youmunan/communty/controller/AuthorizeController.java
+++ src/main/java/top/youmunan/communty/controller/AuthorizeController.java
@@ -10,6 +10,10 @@ import top.youmunan.communty.Provider.GitHubProvider;
 import top.youmunan.communty.dto.AccessTokenDTO;
 import top.youmunan.communty.dto.GitHubUser;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class AuthorizeController {
 
@@ -26,8 +30,9 @@ public class AuthorizeController {
     private String clientSecret;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request, HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setRedirect_uri(redirectId);
         accessTokenDTO.setClient_id(clientId);
@@ -37,6 +42,16 @@ public class AuthorizeController {
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser gitHubUser = gitHubProvider.getGitHubUser(accessToken);
         System.out.println(gitHubUser);
-        return "index";
+
+        if (gitHubUser != null && gitHubUser.getLogin() != null) {
+            // 登录成功
+            HttpSession session = request.getSession();
+            session.setAttribute("user", gitHubUser);
+            // 重定向，改变地址栏地址
+            return "redirect:/";
+        } else {
+            // 登陆失败
+            return "index";
+        }
     }
 }
